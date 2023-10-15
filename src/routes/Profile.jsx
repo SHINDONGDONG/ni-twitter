53eabccf10aa78e482ecd654490dfd80e9c41d98
mincheol <homerjay@naver.com>
2023-10-15 16:14:44 +0900

    db firebase where data get

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,8 +1,8 @@
-import React from "react";
-import fbase from "../fbase";
+import React, { useEffect } from "react";
+import fbase, { dbService } from "../fbase";
 import { useNavigate } from "react-router-dom";
 
-export default function Profile() {
+export default function Profile({ userObj }) {
   const navigate = useNavigate();
 
   const onLogOutClick = () => {
@@ -11,6 +11,19 @@ export default function Profile() {
       .signOut()
       .then((r) => navigate("/"));
   };
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("nweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt")
+      .get();
+
+    console.log(nweets.docs.map((doc) => doc.data()));
+  };
+
+  useEffect(() => {
+    getMyNweets();
+  }, []);
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
